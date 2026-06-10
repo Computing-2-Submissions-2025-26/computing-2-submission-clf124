7f1d820902d78b4ae73cf5a5121c7cfd90a434a9
clf124 <chiara.foschi24@imperial.ac.uk>
2026-06-10 11:53:25 +0100

    fix

--- web-app/ramda.js
+++ web-app/ramda.js
@@ -1,7 +1,3 @@
-/**
- * This is an integration fix, so that Ramda can be imported in the same way
- * both on server and on browser, and also not trip jslint.
- */
 /*jslint-disable*/
 import * as R from "../node_modules/ramda/es/index.js";
 export default R;
